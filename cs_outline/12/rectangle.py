8e564eb2f3471cba44f0dec9feb75309495bd02a
skmtrd <s1f102400314@iniad.org>
2024-07-01 17:58:19 +0900

    feature : 12 / Point class

--- cs_outline/12/rectangle.py
+++ cs_outline/12/rectangle.py
@@ -9,3 +9,15 @@ r = Rectangle(10, 20)
 print(r.area())  # 200
 print(r._width)
 print(r._height)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def distance(self, other):
+        return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+    
+p1 = Point(2.0, 3.0)
+p2 = Point(5.0, 7.0)
+print(p1.distance(p2))  
