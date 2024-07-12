38e8ef35f18898db1ae45d4a289d566cb2bf7014
skmtrd <s1f102400314@iniad.org>
2024-07-12 13:56:39 +0900

    assignment timer

--- cs_exercise/13-2-3/13-2-3/js/assignment.js
+++ cs_exercise/13-2-3/13-2-3/js/assignment.js
@@ -1,19 +1,37 @@
 let count = 0;
 let waittime = 0;
+let interval = null;
+let isActive = false;
 
 function timer() {
+  if (count === waittime) {
+    clearInterval(interval);
+    complite();
+    return;
+  }
   count++;
   let result = document.getElementById("message");
   result.textContent = waittime - count;
 }
 
+function complite() {
+  alert(`${waittime / 60}分経過しました`);
+  isActive = false;
+}
+
 function startNoodle() {
+  if (isActive) return;
+
+  isActive = true;
   let waittimeElement = document.getElementById("time");
   waittime = Number(waittimeElement.value) * 60;
 
   count = 0;
-
-  let interval = setInterval(timer, 1000);
+  interval = setInterval(timer, 1000);
 }
 
-function stopNoodle() {}
+function stopNoodle() {
+  clearInterval(interval);
+  isActive = false;
+  alert("中断しました");
+}
